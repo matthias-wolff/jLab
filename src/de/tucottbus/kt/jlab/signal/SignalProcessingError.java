6e61152f12f433df12546afb88a1ec4b813ba172
matthias-wolff <matthias.wolff@tu-cottbus.de>
2015-04-04 21:43:29 +0200

    * Added jLab kernel

---
+++ src/de/tucottbus/kt/jlab/signal/SignalProcessingError.java
@@ -0,0 +1,12 @@
+package de.tucottbus.kt.jlab.signal;
+
+public class SignalProcessingError extends Error
+{
+  private static final long serialVersionUID = 1L;
+
+  public SignalProcessingError(String message)
+  {
+    super(message);
+  }
+
+}
---
+++ src/de/tucottbus/kt/jlab/signal/SignalProcessingException.java
@@ -0,0 +1,12 @@
+package de.tucottbus.kt.jlab.signal;
+
+public class SignalProcessingException extends Exception
+{
+  private static final long serialVersionUID = 1L;
+
+  public SignalProcessingException(String message)
+  {
+    super(message);
+  }
+
+}
